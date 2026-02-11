fe6395a138c83dafc0a030994d552f119275cdec
RezDead <razorjcgames@gmail.com>
2026-02-11 11:49:11 -0800

    Added error to main

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using std::cout;
 
 int main()
 {
-    cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
+    cout << "THE FIRST EXAMPLE MATH DISPLAY!\n"
     cout << "Hi, please enter two whole numbers: ";
 
     int x,y;
